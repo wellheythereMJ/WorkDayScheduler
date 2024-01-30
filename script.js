6f60475c88d953086d5e627e655e1dcc70da8d0e
wellheythereMJ <abetterplace@outlook.com>
2024-01-29 17:32:06 -0700

    add logic for past, present, future to color code hour blocks

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ $(function () {
   // useful when saving the description in local storage?
   $(".saveBtn").on( "click", function() {
     console.log( $( this ) );
-    var id = $( this ).parent().attr("id")
-    var Event = $( this ).siblings("textarea").val() 
+    var id = $( this ).parent().attr("id");
+    var Event = $( this ).siblings("textarea").val() ;
     localStorage.setItem(id, Event);
   })
   // TODO: Add code to apply the past, present, or future class to each time
@@ -19,11 +19,37 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  //  dayjs().hour()
+    $(".time-block").each(function(){
+      var currentTime = dayjs().hour()
+      var time = $ ( this ).attr("id").slice(5)
+      console.log( currentTime, time )
+      //if else statments
+      // if my time is less than my current time it's in past if equal pres, if greater it's CSSFontFeatureValuesRule
+      // based on that add the class you want to see to add the VideoColorSpace
+      // add class/remove class
+      if (currentTime > time) {
+      $( this ).addClass("past");
+      $( this ).removeClass("future", "present");
+      }
+      if (currentTime == time) {
+      $( this ).addClass("present");
+      $( this ).removeClass("future", "past");
+      }
+      if (currentTime < time) {
+      $( this ).addClass("future");
+      $( this ).removeClass("past", "present");
+      }
+    })
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  $(".time-block").each(function(){
+    console.log( $( this ).attr("id"));
+    var ID = $( this ).attr("id");
+    $(this).children("textarea").val(localStorage.getItem(ID));
+  })
+
   // TODO: Add code to display the current date in the header of the page.
   $("#currentDay").text(dayjs().format('MM/DD/YYYY'))
 });
